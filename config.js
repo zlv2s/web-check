8a8a451ac2c23f9c361b99fba6ff2543858a3ee7
zlv2s <zouli1911@gmail.com>
2021-10-23 00:42:32 +0800

    优化

---
+++ config.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'http://httpbin.org'
+
+module.exports = {
+  BASE_URL
+}
---
+++ request.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+const { BASE_URL } = require('./config')
+
+const service = axios.create({
+  baseURL: BASE_URL,
+  timeout: 8000
+})
+
+service.interceptors.request.use(
+  config => {
+    // todo
+    return config
+  },
+  error => {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
+
+service.interceptors.response.use(
+  response => {
+    if (response.status === 200) {
+      return response.data
+    } else {
+      Promise.reject(response)
+    }
+  },
+  error => {
+    console.log(error.response)
+    return Promise.reject(error.response.data)
+  }
+)
+
+module.exports = service
