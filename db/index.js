1349d00db977ad5fdd7b6194db46c0e6c46df5a3
zlv2s <zouli1911@gmail.com>
2021-11-06 15:16:42 +0800

    优化

--- db/index.js
+++ db/index.js
@@ -1,23 +1,61 @@
 let mysql = require('mysql')
 
-let connection = mysql.createConnection({
-  host: '112.124.67.0',
-  port: '33063',
-  user: 'root',
-  password: 'Syu1314'
+const db = mysql.createConnection({
+    host: '112.124.67.0',
+    port: '33063',
+    user: 'root',
+    password: 'Syu1314'
 })
 
-const query = `select * from \`demo-2\`.gl_data`
+class DB {
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new DB()
+        }
+        return this.instance
+    }
 
-connection.connect(function (err) {
-  if (err) {
-    return console.error('error: ' + err.message)
-  }
+    constructor() {
+        this.db = mysql.createConnection({
+            host: '112.124.67.0',
+            port: '33063',
+            user: 'root',
+            password: 'Syu1314'
+        })
+        this.init()
+    }
 
-  console.log('Connected to the MySQL server.')
-})
+    async init() {
+        const message = await this.connect()
+        console.info(message)
+    }
 
-connection.query(query, (error, rows) => {
-  console.log(error)
-  console.log(JSON.parse(JSON.stringify(rows)))
-})
+    connect() {
+        return new Promise((resolve, reject) => {
+            this.db.connect(function (err) {
+                if (err) {
+                    reject(err.message)
+                }
+                resolve('Connected to the MySQL server')
+            })
+        })
+    }
+
+    query(criteria) {
+        return new Promise((resolve, reject) => {
+            if (typeof criteria !== 'string') {
+                reject('query not correct')
+            }
+            this.db.query(criteria, (err, rows) => {
+                if (err) {
+                    reject(err)
+                }
+                resolve(rows)
+            })
+        })
+    }
+}
+
+
+module.exports = DB.getInstance()
+
---
+++ glData.js
@@ -0,0 +1,89 @@
+const db = require('./db')
+
+const query = `select * from \`demo-2\`.gl_data`
+
+function statByLoanId(rowsArr) {
+    const map = new Map()
+    rowsArr.map(x => {
+        x.business_date = x.business_date.split('T')[0]
+        return x
+    }).forEach(row => {
+        if (map.has(row.loan_id)) {
+            const exist = map.get(row.loan_id)
+            if (!exist[row.business_date]) {
+                exist[row.business_date] = {}
+                exist[row.business_date][`${row.txn_code}_${row.txn_desc}`] = [row.amount]
+            } else {
+                if (exist[row.business_date][`${row.txn_code}_${row.txn_desc}`]) {
+                    exist[row.business_date][`${row.txn_code}_${row.txn_desc}`].push(row.amount)
+                } else {
+                    exist[row.business_date][`${row.txn_code}_${row.txn_desc}`] = [row.amount]
+                }
+            }
+        } else {
+            const obj = {}
+            map.set(row.loan_id, obj)
+            obj[row.business_date] = {}
+            obj[row.business_date][`${row.txn_code}_${row.txn_desc}`] = [row.amount]
+        }
+    })
+    console.log(JSON.stringify(Object.fromEntries(map.entries())))
+}
+
+
+function proxyObj(obj) {
+    return new Proxy(obj, {
+        get(target, property) {
+            if (property === 'total') {
+                return target['amount'].reduce((ac, num) => ac + num, 0).toFixed(2)
+            }
+            return target[property]
+        },
+        set(target, property, value) {
+            target[property] = value
+            return true
+        }
+    })
+}
+
+function statByBusiDate(rowsArr) {
+    const map = new Map()
+    rowsArr.map(x => {
+        x.business_date = x.business_date.split('T')[0]
+        return x
+    }).forEach(row => {
+        if (map.has(row.business_date)) {
+            const exist = map.get(row.business_date)
+            if (exist[`${row.txn_code}_${row.txn_desc}`]) {
+                exist[`${row.txn_code}_${row.txn_desc}`]['amount'].push(row.amount)
+            } else {
+                exist[`${row.txn_code}_${row.txn_desc}`] = proxyObj({
+                    amount: [row.amount],
+                    total: 0
+                })
+            }
+        } else {
+            const obj = {}
+            map.set(row.business_date, obj)
+            obj[`${row.txn_code}_${row.txn_desc}`] = proxyObj({
+                amount: [row.amount],
+                total: 0
+            })
+        }
+    })
+    console.log(JSON.stringify(Object.fromEntries(map.entries())))
+}
+
+async function main() {
+    try {
+        const rows = await db.query(query)
+        const rowsArr = JSON.parse(JSON.stringify(rows))
+        // statByLoanId(rowsArr)
+        statByBusiDate(rowsArr)
+    } catch (e) {
+        console.error('caught error: ', e)
+    }
+}
+
+
+main()
